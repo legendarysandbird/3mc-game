7fd9c5b39d290db179da0b67100d018084531bdf
SweatyCarrot <cosmith541@gmail.com>
2025-04-01 16:48:46 -0700

    update player_animation.gd > animate() logic to work with idle animation.

--- scripts/player_animation.gd
+++ scripts/player_animation.gd
@@ -2,21 +2,24 @@ extends AnimatedSprite2D
 @export var player: Node
 
 func _ready() -> void:
-	set_animation("still")
+	set_animation("idle")
 
 func animate() -> void:
 	if !Input.is_anything_pressed():
-		set_animation("still")
+		set_animation("idle")
+		play()
 	
 	if Input.is_action_pressed("player_left") != Input.is_action_pressed("player_right"): # Poor man's XOR
-		set_animation("walk")
 		if player.velocity.y != 0:
-			stop()
-		if player.velocity.x != 0:
+			set_animation("idle")
+			flip_h = player.velocity.x < 0
+		elif player.velocity.x != 0:
+			set_animation("walk")
 			flip_h = player.velocity.x < 0
 		play()
 	else:
-		stop()
+		set_animation("idle")
+		play()
 
 func _process(_delta: float) -> void:
 		animate()
