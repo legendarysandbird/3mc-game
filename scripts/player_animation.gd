e07592e09af4b72dbb0b8181682f99d95c782d9d
SweatyCarrot <sweatycontroles@gmail.com>
2025-04-04 12:42:36 -0700

    Refactor animate()

--- scripts/player_animation.gd
+++ scripts/player_animation.gd
@@ -2,24 +2,29 @@ extends AnimatedSprite2D
 @export var player: Node
 
 func _ready() -> void:
-	set_animation("idle")
+	play()
 
 func animate() -> void:
-	if !Input.is_anything_pressed():
+	if !player.is_on_floor():
 		set_animation("idle")
-		play()
+		if !flip_h:
+			flip_h = Input.is_action_pressed("player_left")
+		else:
+			flip_h = Input.is_action_pressed("player_left")
+		return
 	
-	if Input.is_action_pressed("player_left") != Input.is_action_pressed("player_right"): # Poor man's XOR
-		if player.velocity.y != 0:
-			set_animation("idle")
-			flip_h = player.velocity.x < 0
-		elif player.velocity.x != 0:
-			set_animation("walk")
-			flip_h = player.velocity.x < 0
-		play()
+	if player.velocity.x == 0 and !Input.is_anything_pressed():
+		set_animation("idle")
+		return
+	
+	if Input.is_action_pressed("player_left") != Input.is_action_pressed("player_right"):
+		set_animation("walk")
 	else:
 		set_animation("idle")
-		play()
+		return
+	
+	flip_h = Input.is_action_pressed("player_left")
+
 
 func _process(_delta: float) -> void:
 		animate()
