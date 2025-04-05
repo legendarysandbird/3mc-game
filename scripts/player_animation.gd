3f18b99ce1502088661c9f114e7a5653edfb3196
SweatyCarrot <cosmith541@gmail.com>
2025-04-05 14:51:29 -0700

    remove redundant logic

--- scripts/player_animation.gd
+++ scripts/player_animation.gd
@@ -2,14 +2,10 @@ extends AnimatedSprite2D
 @export var player: Node
 
 func _ready() -> void:
-	assert(is_instance_valid(player), "player_animation.gd > _ready() - player instance is not valid")
+	assert(is_instance_valid(player), "Player instance is not valid")
 	play()
 
 func animate() -> void:
-	if player.velocity.x == 0 and !Input.is_anything_pressed():
-		set_animation("idle")
-		return
-	
 	if Input.is_action_pressed("player_left") != Input.is_action_pressed("player_right"):
 		set_animation("walk")
 	else:
