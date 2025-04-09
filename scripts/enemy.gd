6b70d1e2afc651041ba7110d74acfcb33db88869
Brandon Dodd <legendarysandbird@gmail.com>
2025-04-08 19:24:59 -0700

    Validate all references

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,7 +2,7 @@ class_name Enemy extends CharacterBody2D
 
 @export var speed: float = 100.0
 
-@onready var player: Player = get_player_node()
+@onready var player: Player = get_tree().get_first_node_in_group(Groups.PLAYER)
 @onready var detectionArea: Area2D = $DetectionDistance
 @onready var deathArea: Area2D = $DeathCollision
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -11,6 +11,9 @@ var calculated_velocity: Vector2
 var player_detected: bool
 
 func _ready() -> void:
+	for node: Node2D in [player, detectionArea, deathArea]:
+		assert(is_instance_valid(node))
+
 	add_to_group(Groups.ENEMY)
 	detectionArea.body_entered.connect(on_distance_detection_body_entered)
 	detectionArea.body_exited.connect(on_distance_detection_body_exited)
@@ -28,12 +31,6 @@ func calculate_horizontal_movement() -> float:
 		return 0
 	
 	return -speed if global_position.x > player.global_position.x else speed
-
-func get_player_node() -> Player:
-	var player_node: Player = get_tree().get_first_node_in_group(Groups.PLAYER)
-	assert(is_instance_valid(player_node))
-	
-	return player_node
 
 func on_distance_detection_body_entered(body: Node2D) -> void:
 	check_and_set_player_detected(body, true)
