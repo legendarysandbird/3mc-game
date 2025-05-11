349ddeb32d7500f39526d3293d858b2879f29b83
SweatyCarrot <cosmith541@gmail.com>
2025-05-11 15:24:50 -0700

    add health bar

--- scripts/logic/health.gd
+++ scripts/logic/health.gd
@@ -1,10 +1,13 @@
 class_name Health extends Node
-@export var health: int
-@onready var maxHealth: int = health
+@export var health_pool: int
+@onready var max_health: int = health_pool
 
 signal death
+signal health_pool_changed
 
 func change_health(value: int) -> void:
-	health = clamp(health + value, 0, maxHealth)
-	if health == 0:
+	health_pool = clamp(health_pool + value, 0, max_health)
+	if health_pool == 0:
 		death.emit()
+	
+	health_pool_changed.emit()
---
+++ scripts/logic/health_bar.gd
@@ -0,0 +1,11 @@
+extends ProgressBar
+
+@export var health_pool: Health
+
+func _ready() -> void:
+	health_pool.health_pool_changed.connect(on_health_pool_changed)
+	max_value = health_pool.max_health
+	value = health_pool.health_pool
+	
+func on_health_pool_changed() -> void:
+	value = health_pool.health_pool
