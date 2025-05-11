c44963dec7d72da9eff0055c48e9e3a62a907d48
SweatyCarrot <cosmith541@gmail.com>
2025-05-11 14:33:20 -0700

    add player and enemy interaction logic

--- scripts/enemy/enemy.gd
+++ scripts/enemy/enemy.gd
@@ -1,24 +1,25 @@
 class_name Enemy extends CharacterBody2D
 
 @export var speed: float = 100.0
+@export var contact_damage: int = 1
 
 @onready var player: Player = get_tree().get_first_node_in_group(Groups.PLAYER)
 @onready var detectionArea: Area2D = $DetectionDistance
-@onready var deathArea: Area2D = $DeathCollision
-@onready var healthPool: Node = $Health
+@onready var hitbox: Area2D = $Hitbox
+@onready var health_pool: Health = $Health
 
 var calculated_velocity: Vector2
 var player_detected: bool
 
 func _ready() -> void:
-	for node: Node in [player, detectionArea, deathArea, healthPool]:
+	for node: Node in [player, detectionArea, hitbox, health_pool]:
 		assert(is_instance_valid(node))
 
 	add_to_group(Groups.ENEMY)
 	detectionArea.body_entered.connect(on_distance_detection_body_entered)
 	detectionArea.body_exited.connect(on_distance_detection_body_exited)
-	deathArea.body_entered.connect(on_death_detection_body_entered)
-	healthPool.death.connect(on_health_pool_death)
+	hitbox.area_entered.connect(on_hitbox_area_entered)
+	health_pool.death.connect(on_health_pool_death)
 
 func _physics_process(_delta: float) -> void:
 	velocity = calculate_velocity()
@@ -41,9 +42,10 @@ func check_and_set_player_detected(body: Node2D, is_player_detected: bool) -> vo
 	if body.is_in_group(Groups.PLAYER):
 		player_detected = is_player_detected
 
-func on_death_detection_body_entered(body: Node2D) -> void:
-	if body.is_in_group(Groups.PLAYER):
-		healthPool.change_health(-1)
+func on_hitbox_area_entered(area: Node2D) -> void:
+	if area.is_in_group(Groups.PLAYER_PROJECTILE):
+		health_pool.change_health(area.damage)
+		area.queue_free()
 
 func on_health_pool_death() -> void:
 	print("enemy died")
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -7,11 +7,17 @@ var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var rotation_speed: int = 30
 @export var projectile_speed: float = 300.0
 
-@onready var projectile_spawn_node: Node2D = $AnimatedSprite2D/RotationPoint/Arm
+@onready var projectile_spawn_node: Node2D = $AnimatedSprite2D/RotationPoint/Arm/ProjectileSpawnPoint
+@onready var hitbox: Node2D = $Hitbox
+@onready var health_pool: Health = $Health
 
 func _ready() -> void:
-	assert(is_instance_valid(projectile_spawn_node))
+	for node: Node in [projectile_spawn_node, hitbox, health_pool]:
+		assert(is_instance_valid(node))
+	
 	add_to_group(Groups.PLAYER)
+	hitbox.body_entered.connect(on_hitbox_body_entered)
+	health_pool.death.connect(on_health_pool_death)
 
 func player_rotate(delta: float) -> void:
 	var target_rotation: float = 0
@@ -26,16 +32,11 @@ func is_jump_eligible() -> bool:
 	
 
 func _physics_process(delta: float) -> void:
+	#Movement Region
 	velocity.x = 0
 	
 	if !is_on_floor():
 		velocity.y += gravity * delta
-	
-	if Input.is_action_just_pressed("fire"):
-		var spawn_position: Vector2 = projectile_spawn_node.global_position
-		var projectile_direction: Vector2 = (get_global_mouse_position() - spawn_position).normalized()
-		var projectile: Projectile = Projectile.Create(projectile_spawn_node.global_position, projectile_direction * projectile_speed)
-		get_tree().root.add_child(projectile)
 
 	if Input.is_action_pressed("player_jump") and is_jump_eligible():
 		velocity.y -= JUMP_VELOCITY
@@ -45,6 +46,23 @@ func _physics_process(delta: float) -> void:
 		velocity.x -= MOVE_SPEED
 	if Input.is_action_pressed("player_right"):
 		velocity.x += MOVE_SPEED
-	
+		
 	move_and_slide()
 	player_rotate(delta)
+		
+	#Shooting Region
+	if Input.is_action_just_pressed("fire"):
+		var spawn_position: Vector2 = projectile_spawn_node.global_position
+		var projectile_direction: Vector2 = (get_global_mouse_position() - spawn_position).normalized()
+		var projectile: Projectile = Projectile.Create(projectile_spawn_node.global_position, projectile_direction * projectile_speed)
+		get_tree().root.add_child(projectile)
+		projectile.add_to_group(Groups.PLAYER_PROJECTILE)
+
+func on_hitbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group(Groups.ENEMY):
+		print("player hit")
+		health_pool.change_health(-1)
+
+func on_health_pool_death() -> void:
+	print("You died!")
+	queue_free()
