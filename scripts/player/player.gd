0362d1815bcf114b645c003eb56ef7ddab70b567
SweatyCarrot <cosmith541@gmail.com>
2025-05-14 17:36:20 -0700

    Separate Player._physics_process() logic into two functions

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -10,9 +10,10 @@ var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var projectile_spawn_node: Node2D = $AnimatedSprite2D/Arm/ProjectileSpawnPoint
 @onready var hitbox: Node2D = $Hitbox
 @onready var health_pool: Health = $Health
+@onready var jump_timer: Timer = $JumpTimer
 
 func _ready() -> void:
-	for node: Node in [projectile_spawn_node, hitbox, health_pool]:
+	for node: Node in [projectile_spawn_node, hitbox, health_pool, jump_timer]:
 		assert(is_instance_valid(node))
 	
 	add_to_group(Groups.PLAYER)
@@ -28,11 +29,9 @@ func player_rotate(delta: float) -> void:
 	
 	
 func is_jump_eligible() -> bool:
-	return is_on_floor() and $JumpTimer.time_left == 0
+	return is_on_floor() and jump_timer.time_left == 0
 	
-
-func _physics_process(delta: float) -> void:
-	#Movement Region
+func player_movement(delta: float) -> void:
 	velocity.x = 0
 	
 	if !is_on_floor():
@@ -40,7 +39,7 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_pressed("player_jump") and is_jump_eligible():
 		velocity.y -= JUMP_VELOCITY
-		$JumpTimer.start()
+		jump_timer.start()
 		
 	if Input.is_action_pressed("player_left"):
 		velocity.x -= MOVE_SPEED
@@ -49,14 +48,18 @@ func _physics_process(delta: float) -> void:
 		
 	move_and_slide()
 	player_rotate(delta)
-		
-	#Shooting Region
+
+func player_shoot() -> void:
 	if Input.is_action_just_pressed("fire"):
 		var spawn_position: Vector2 = projectile_spawn_node.global_position
 		var projectile_direction: Vector2 = (get_global_mouse_position() - spawn_position).normalized()
 		var projectile: Projectile = Projectile.Create(projectile_spawn_node.global_position, projectile_direction * projectile_speed)
 		get_tree().root.add_child(projectile)
 		projectile.add_to_group(Groups.PLAYER_PROJECTILE)
+
+func _physics_process(delta: float) -> void:
+	player_movement(delta)
+	player_shoot()
 
 func on_hitbox_body_entered(body: Node2D) -> void:
 	if body.is_in_group(Groups.ENEMY):
