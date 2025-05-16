a8531f4d7c99d79a1e6aaf0b48342117735b3bc7
Connor Smith <142857338+SweatyCarrot@users.noreply.github.com>
2025-05-15 20:43:33 -0700

    Implement Health System and Player/Enemy Interaction (#25)

--- scripts/auto-loads/groups.gd
+++ scripts/auto-loads/groups.gd
@@ -2,3 +2,4 @@ class_name Groups extends Node
 
 const PLAYER = "player"
 const ENEMY = "enemy"
+const PLAYER_PROJECTILE = "player_projectile"
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -7,11 +7,18 @@ var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var rotation_speed: int = 30
 @export var projectile_speed: float = 300.0
 
-@onready var projectile_spawn_node: Node2D = $AnimatedSprite2D/RotationPoint/Arm
+@onready var projectile_spawn_node: Node2D = $AnimatedSprite2D/Arm/ProjectileSpawnPoint
+@onready var hitbox: Node2D = $Hitbox
+@onready var health_pool: Health = $Health
+@onready var jump_timer: Timer = $JumpTimer
 
 func _ready() -> void:
-	assert(is_instance_valid(projectile_spawn_node))
+	for node: Node in [projectile_spawn_node, hitbox, health_pool, jump_timer]:
+		assert(is_instance_valid(node))
+	
 	add_to_group(Groups.PLAYER)
+	hitbox.body_entered.connect(on_hitbox_body_entered)
+	health_pool.death.connect(on_health_pool_death)
 
 func player_rotate(delta: float) -> void:
 	var target_rotation: float = 0
@@ -22,29 +29,43 @@ func player_rotate(delta: float) -> void:
 	
 	
 func is_jump_eligible() -> bool:
-	return is_on_floor() and $JumpTimer.time_left == 0
+	return is_on_floor() and jump_timer.time_left == 0
 	
-
-func _physics_process(delta: float) -> void:
+func player_movement(delta: float) -> void:
 	velocity.x = 0
 	
 	if !is_on_floor():
 		velocity.y += gravity * delta
-	
-	if Input.is_action_just_pressed("fire"):
-		var spawn_position: Vector2 = projectile_spawn_node.global_position
-		var projectile_direction: Vector2 = (get_global_mouse_position() - spawn_position).normalized()
-		var projectile: Projectile = Projectile.Create(projectile_spawn_node.global_position, projectile_direction * projectile_speed)
-		get_tree().root.add_child(projectile)
 
 	if Input.is_action_pressed("player_jump") and is_jump_eligible():
 		velocity.y -= JUMP_VELOCITY
-		$JumpTimer.start()
+		jump_timer.start()
 		
 	if Input.is_action_pressed("player_left"):
 		velocity.x -= MOVE_SPEED
 	if Input.is_action_pressed("player_right"):
 		velocity.x += MOVE_SPEED
-	
+		
 	move_and_slide()
 	player_rotate(delta)
+
+func player_shoot() -> void:
+	if Input.is_action_just_pressed("fire"):
+		var spawn_position: Vector2 = projectile_spawn_node.global_position
+		var projectile_direction: Vector2 = (get_global_mouse_position() - spawn_position).normalized()
+		var projectile: Projectile = Projectile.Create(projectile_spawn_node.global_position, projectile_direction * projectile_speed)
+		get_tree().root.add_child(projectile)
+		projectile.add_to_group(Groups.PLAYER_PROJECTILE)
+
+func _physics_process(delta: float) -> void:
+	player_movement(delta)
+	player_shoot()
+
+func on_hitbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group(Groups.ENEMY):
+		print("player hit")
+		health_pool.change_health(-1)
+
+func on_health_pool_death() -> void:
+	print("You died!")
+	queue_free()
