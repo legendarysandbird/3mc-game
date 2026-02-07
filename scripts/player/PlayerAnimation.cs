dec0572c3023e08000c0b1815e46fcd308065d56
Brandon Dodd <legendarysandbird@gmail.com>
2026-02-07 07:58:44 -0800

    Add controller support

--- scripts/player/PlayerAnimation.cs
+++ scripts/player/PlayerAnimation.cs
@@ -3,19 +3,32 @@ using Godot;
 public partial class PlayerAnimation : AnimatedSprite2D
 {
     private float _xScaleCache;
+    [Export] private Node2D? _armNode;
+    [Export] private Player? _playerNode;
 
     public override void _Ready()
     {
+        _armNode = GetNode<Node2D>("Arm").NotNull(nameof(_armNode));
+        _playerNode = GetParent<Player>().NotNull(nameof(_armNode));
         _xScaleCache = Scale.X;
         Play();
     }
 
     public override void _Process(double delta)
     {
-        Animate();
+        SetAnimation();
+        AnimateArm();
     }
 
-    private void Animate()
+    private void AnimateArm()
+    {
+        _armNode.NotNull(nameof(_armNode));
+        _playerNode.NotNull(nameof(_playerNode));
+
+        _armNode.LookAt(_armNode.GlobalPosition + _playerNode.ProjectileDirection);
+    }
+
+    private void SetAnimation()
     {
         if (Input.IsActionPressed("player_left") != Input.IsActionPressed("player_right"))
         {
