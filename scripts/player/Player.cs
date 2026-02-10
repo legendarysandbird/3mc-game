794a6e5c87703b2badd87ecc1a2ec8857df71c73
legendarysandbird <66805046+legendarysandbird@users.noreply.github.com>
2026-02-10 00:35:19 +0000

    Add controller support (#44)

--- scripts/player/Player.cs
+++ scripts/player/Player.cs
@@ -16,6 +16,11 @@ public partial class Player : CharacterBody2D
     private Health? _healthPool;
     private Timer? _jumpTimer;
     private Timer? _gunTimer;
+    private Vector2 _mousePosition;
+    private Vector2 _stickDirection;
+
+    public Vector2 ProjectileDirection { get; private set; }
+
 
     public override void _Ready()
     {
@@ -24,6 +29,8 @@ public partial class Player : CharacterBody2D
         _healthPool = GetNode<Health>("Health").NotNull(nameof(_healthPool));
         _jumpTimer = GetNode<Timer>("JumpTimer").NotNull(nameof(_jumpTimer));
         _gunTimer = GetNode<Timer>("GunTimer").NotNull(nameof(_gunTimer));
+        _mousePosition = GetGlobalMousePosition();
+        ProjectileDirection = Vector2.Right;
 
         _hitbox.BodyEntered += OnHitboxBodyEntered;
         _healthPool.HealthEmpty += OnHealthPoolEmpty;
@@ -66,15 +73,7 @@ public partial class Player : CharacterBody2D
             _jumpTimer.Start();
         }
 
-        if (Input.IsActionPressed("player_left"))
-        {
-            x -= MoveSpeed;
-        }
-
-        if (Input.IsActionPressed("player_right"))
-        {
-            x += MoveSpeed;
-        }
+        x = Input.GetAxis("player_left", "player_right") * MoveSpeed;
 
         Velocity = new Vector2(x, y);
         MoveAndSlide();
@@ -86,6 +85,13 @@ public partial class Player : CharacterBody2D
         _gunTimer.NotNull(nameof(_gunTimer));
         _projectileSpawnNode.NotNull(nameof(_projectileSpawnNode));
 
+        Vector2 spawnPosition = _projectileSpawnNode.GlobalPosition;
+        Vector2 projectileDirection = GetProjectileDirection(spawnPosition);
+        if (projectileDirection != Vector2.Zero)
+        {
+            ProjectileDirection = projectileDirection;
+        }
+
         if (!Input.IsActionPressed("fire") || _gunTimer.TimeLeft > 0)
         {
             return;
@@ -93,11 +99,30 @@ public partial class Player : CharacterBody2D
 
         _gunTimer.Start();
 
-        Vector2 spawnPosition = _projectileSpawnNode.GlobalPosition;
-        Vector2 projectileDirection = (GetGlobalMousePosition() - spawnPosition).Normalized();
-        Projectile projectile = Projectile.Create(_projectileSpawnNode.GlobalPosition, projectileDirection * _projectileSpeed);
+        Projectile projectile = Projectile.Create(_projectileSpawnNode.GlobalPosition, ProjectileDirection * _projectileSpeed);
 
         GetTree().Root.AddChild(projectile);
+    }
+
+    private Vector2 GetProjectileDirection(Vector2 spawnPosition)
+    {
+        var projectileDirection = Vector2.Zero;
+
+        var mousePosition = GetGlobalMousePosition();
+        if (_mousePosition != mousePosition)
+        {
+            projectileDirection = mousePosition - spawnPosition;
+            _mousePosition = mousePosition;
+        }
+
+        var stickDirection = Input.GetVector("player_aim_left", "player_aim_right", "player_aim_up", "player_aim_down");
+        if (_stickDirection != stickDirection)
+        {
+            projectileDirection = stickDirection;
+            _stickDirection = stickDirection;
+        }
+
+        return projectileDirection.Normalized();
     }
 
     private void OnHitboxBodyEntered(Node2D body)
