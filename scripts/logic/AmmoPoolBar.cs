f955e69236647b95d9fa364c502d0417a51207d6
Connor Smith <142857338+SweatyCarrot@users.noreply.github.com>
2026-02-14 09:38:42 -0800

    Add Ammo Pool (#45)
    * Reset main_menu.tcsn to be main scene
    
    * Add ammo_pool.tcsn and AmmoPool.cs. I don't think I modified as many files as Git thinks I did... Probab'y something to do with opening them in Godot 4.6 for the first time?
    
    * Add working AmmoPool and AmmoPoolBar
    
    * Now with 100% more .NET format!

---
+++ scripts/logic/AmmoPoolBar.cs
@@ -0,0 +1,22 @@
+using Godot;
+
+public partial class AmmoPoolBar : ProgressBar
+{
+
+    [Export] private AmmoPool? _ammoPool;
+
+    public override void _Ready()
+    {
+        _ammoPool.NotNull(nameof(_ammoPool));
+
+        _ammoPool.AmmoPoolValueChanged += OnAmmoPoolValueChanged;
+        MaxValue = _ammoPool.MaxAmmo;
+        Value = _ammoPool.AmmoPoolValue;
+    }
+
+    private void OnAmmoPoolValueChanged()
+    {
+        _ammoPool.NotNull(nameof(_ammoPool));
+        Value = _ammoPool.AmmoPoolValue;
+    }
+}
