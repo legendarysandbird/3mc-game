6a326db1c8873ed9158a1f46ae5484fdc1551d1f
SweatyCarrot <cosmith541@gmail.com>
2026-02-19 19:12:13 -0800

    Implement PlayerHud without using 'Editable Children'

--- scripts/logic/HealthBar.cs
+++ scripts/logic/HealthBar.cs
@@ -2,10 +2,25 @@ using Godot;
 
 public partial class HealthBar : ProgressBar
 {
-    [Export] private Health? _healthPool;
+
+    [Export] private Node? _healthPoolConnection;
+
+    private Health? _healthPool;
 
     public override void _Ready()
     {
+        _healthPoolConnection.NotNull(nameof(_healthPoolConnection));
+
+        if (_healthPoolConnection is PlayerHud)
+        {
+            PlayerHud? playerHud = _healthPoolConnection.NotNull(nameof(_healthPoolConnection)) as PlayerHud;
+            _healthPool = playerHud.NotNull(nameof(playerHud)).Player.NotNull(nameof(Player)).GetNode<Health>("Health") as Health;
+        }
+        else
+        {
+            _healthPool = _healthPoolConnection as Health;
+        }
+
         _healthPool.NotNull(nameof(_healthPool));
 
         _healthPool.HealthPoolChanged += OnHealthPoolChanged;
