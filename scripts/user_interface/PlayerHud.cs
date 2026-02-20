9a19c29558cd4c46b05bee99fe59cddd02d004b2
SweatyCarrot <cosmith541@gmail.com>
2026-02-19 20:16:27 -0800

    Clean up PlayerHud.cs

--- scripts/user_interface/PlayerHud.cs
+++ scripts/user_interface/PlayerHud.cs
@@ -5,13 +5,10 @@ public partial class PlayerHud : Control
 {
 
     [Export] public Player? Player { get; private set; }
-    // Called when the node enters the scene tree for the first time.
+
     public override void _Ready()
     {
+        Player.NotNull(nameof(Player));
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
 }
